f0c33d22e92f9519ee2f0659b82149686bb8e738
Kim KyoungHo <super-alan@nate.com>
2017-09-13 19:06:45 +0900

    Refresh

--- Java/Test/src/test01/test01.java
+++ Java/Test/src/test01/test01.java
@@ -1,5 +1,12 @@
 package test01;
 
 public class test01 {
-
+	public static void main(String[] args) {
+		int[][] score = {
+				{100, 60, 40, 20},
+				{90, 50, 20, 10},
+				{100, 60, 40, 20}
+		};
+	}
+	
 }
